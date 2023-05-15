2925666adc35364eb9c11691f6175bce5b48b706
Partial <133568898+Partial206@users.noreply.github.com>
2023-05-15 12:05:39 +0300

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class (Your file name) {
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      scan.useLocale(Locale.US);
+
+      System.out.println("Select a process 1= Divide 2= impact 3= addition 4= extraction");
+
+      int process = scan.nextInt();
+      System.out.println("Select Number 1");
+      int number1 = scan.nextInt();
+      System.out.println("Select Number 2");
+      int number2 = scan.nextInt();
+      if(process == 1){
+        System.out.println((double)(number1/number2));
+      }
+      if(process == 2){
+        System.out.println((double)(number1*number2));
+      }
+      if(process == 3){
+        System.out.println((double)(number1+number2));
+      }
+      if(process == 4){
+        System.out.println((double)(number1-number2));
+      }
+      else{
+        System.out.println("Error");
+      }
+    }
+}
