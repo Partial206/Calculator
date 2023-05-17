b44cf6951e3fd03306ba1f4dedb821a7984a7eda
Partial <133568898+Partial206@users.noreply.github.com>
2023-05-17 16:48:20 +0300

    Update calculator.java
    no more fixes

--- calculator.java
+++ calculator.java
@@ -1,35 +1,33 @@
 import java.util.Scanner;
-import java.util.Locale;
 
-public class calculator {
+public class scalculator {
     public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in);
-      scan.useLocale(Locale.US);
+       Scanner scan = new Scanner(System.in);
 
-      System.out.println("Select a process 1= Divide 2= impact 3= addition 4= extraction");
+       System.out.println("Select a process 1= Divide 2= impact 3= addition 4= extraction");
+       int process = scan.nextInt();
 
-      int process = scan.nextInt();
-      System.out.println("Select Number 1");
-      int number1 = scan.nextInt();
-      System.out.println("Select Number 2");
-      int number2 = scan.nextInt();
-      if(process == 1){
-        System.out.println((double)(number1/number2));
-      }
-      if(process == 2){
-        System.out.println((double)(number1*number2));
-      }
-      if(process == 3){
-        System.out.println((double)(number1+number2));
-      }
-      if(process == 4){
-        System.out.println((double)(number1-number2));
-      }
-      if(process > 4){
-        System.out.println("Error");
-      }
-      if(process < 1){
-        System.out.println("Error");
-      }
+       System.out.println("Select Number 1");
+       int number1 = scan.nextInt();
+
+       System.out.println("Select Number 2");
+       int number2 = scan.nextInt();
+
+       switch(process){
+        case 1:
+         System.out.println((double)(number1/number2));
+        break;      
+        case 2:
+         System.out.println((double)(number1*number2));;
+        break;       
+        case 3:
+         System.out.println((double)(number1+number2));;
+        break;
+        case 4:
+         System.out.println((double)(number1-number2));
+        break;
+        default:
+         System.out.println("Error");
+       }      
     }
 }
